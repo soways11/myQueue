8af2894f0d2a7d5d7f5e3a7a78b3ad4d32ffee3d
soways11 <157223054+soways11@users.noreply.github.com>
2026-06-14 12:37:19 +0300

    Update myQueue.cpp

--- myQueue.cpp
+++ myQueue.cpp
@@ -1,4 +1,4 @@
-template <typename type> struct element{
+template <typename type> struct element{ // struct, that will be used as element of stack
     element* pointer;
     type value;
 };
@@ -8,14 +8,14 @@ template <typename type> class queue{
     element<type>* lastElement;
     long long int size;
   public:
-    queue();
-    ~queue();
-    void push(type x);
-    void pop();
-    type front();
-    type back();
-    long long int getSize();
-    bool empty();
+    queue(); // constructor
+    ~queue(); // destructor
+    void push(type x ); // add new element to the end of the queue
+    void pop(); // delete first element
+    type front(); // return first element of queue
+    type back(); // return last element of queue
+    long long int getSize(); // return size
+    bool empty(); // check if array is empty
 };
 template <typename type> queue<type>::queue(){
     firstElement = nullptr;
@@ -23,33 +23,43 @@ template <typename type> queue<type>::queue(){
     size = 0;
 }
 template <typename type> queue<type>::~queue(){
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++){ // delete size elements, because queue include size elements, when we delete size elements, queue becomes empty
         element<type>* cur = firstElement;
         firstElement = (*cur).pointer;
-        delete cur;
+        delete cur; // deallocate memory of first element
     }
 }
 template <typename type> void  queue<type>::push(type x){
-    element<type>* cur = new element<type>;
-    (*cur).value = x;
-    (*cur).pointer = nullptr;
-    if (size > 0){
+    if (size <= 0){ // if size == 0, i have to set the same value for lastElement and firstElement
+        element<type>* cur = new element<type>; // allocate memory for new element
+        (*cur).value = x;
+        (*cur).pointer = nullptr;
+        firstElement = cur;
+        lastElement = cur;
+        size++;
+    }else{
+        element<type>* cur = new element<type>; // allocate memory for new element
+        (*cur).value = x;
+        (*cur).pointer = nullptr;
         (*lastElement).pointer = cur;
+        lastElement = cur;
+        size++;
     }
-    lastElement = cur;
-    if (size == 0){
-        firstElement = lastElement;
-    }
-    size++;
 }
 template <typename type> void queue<type>::pop(){
-    element<type>* cur = firstElement;
-    firstElement = (*cur).pointer;
-    delete cur;
-    if (size <= 1){
+    if (size <= 0){ // if size == 0 return
+        return;
+    }else if (size == 1){ // if size == 1, queue becomes void
+        delete firstElement; // deallocate memory of first element
+        firstElement = nullptr;
         lastElement = nullptr;
+        size = 0;
+    }else{
+        element<type>* cur = firstElement;
+        firstElement = (*cur).pointer;
+        delete cur; // deallocate memory of first element
+        size = size - 1;
     }
-    size--;
 }
 template <typename type> type queue<type>::front(){
     return (*firstElement).value;
